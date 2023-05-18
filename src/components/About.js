d8e69cc8fbac699c14a9a82dc905eff078d9b2d3
md-foram-dalsaniya <122962531+md-foram-dalsaniya@users.noreply.github.com>
2023-05-18 16:36:45 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div><font color="white"><b>A notebook is a book or stack of paper pages that are often ruled and used for purposes such as note-taking, 
+    journaling or other writing, drawing, or scrapbooking. Notebooks this year became a lesson in letting go (for me) and in breaking the rules
+  (for my students). No longer did I want to see neat entries, nor did I want to see the same thing inside each notebook! I want to see the messy thinking.
+  I want to see scribbles and doodles and color and pencil and them…I want to see my students on the pages.</b></font></div>
+  )
+}
+
+export default About
